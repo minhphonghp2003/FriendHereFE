ca1c72cb0ecf18ecc3dc1b5440d4b4124277e070
Nguyen Minh Phong <73019986+minhphonghp2003@users.noreply.github.com>
2026-08-17 08:35:58 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,7 +48,7 @@ export const viewport: Viewport = {
   maximumScale: 1,
   userScalable: false,
   viewportFit: "cover",
-  interactiveWidget: "resizes-content",
+
   themeColor: [
     { media: "(prefers-color-scheme: light)", color: "#2BB0AF" },
     { media: "(prefers-color-scheme: dark)", color: "#7DDED0" },
